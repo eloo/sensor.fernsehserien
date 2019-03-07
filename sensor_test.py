8d67e33de60b2bb7df9899a8e95f0a5fa42a9c71
Joseph Weigl <fents@gmx.net>
2019-03-07 22:56:44 +0100

    Create simple test

---
+++ sensor_test.py
@@ -0,0 +1,28 @@
+import unittest
+import requests
+import custom_components.fernsehserien.sensor as sensor
+
+class TestFernsehserien(unittest.TestCase):
+    """
+    Test the parser
+    """
+
+    test_data = [ 'doctor-who-2005', 'the-big-bang-theory', 'suits', 'vikings', 'hubert-ohne-staller', 'game-of-thrones', 'young-sheldon']
+
+    def test_parse(self):
+        """
+        The actual test.
+        Any method which starts with ``test_`` will considered as a test case.
+        """
+        for show in self.test_data:
+            print("Test show: " + show)
+            api_response = requests.get(sensor.BASE_URL.format(show), timeout=10)
+            show_data = sensor.parseResponse(api_response)
+            # print(show_data)
+            self.assertIn('fanart', show_data)
+            self.assertIn('title', show_data)
+            self.assertIn('episodes', show_data)
+            self.assertGreaterEqual(len(show_data['episodes']), 0)
+
+if __name__ == '__main__':
+    unittest.main()
