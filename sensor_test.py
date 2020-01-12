5cde5b45161c9c2f1a6df564b227ccfd4386c6a0
Joseph Weigl <fents@gmx.net>
2020-01-12 17:37:21 +0100

    Fix tests

--- sensor_test.py
+++ sensor_test.py
@@ -54,17 +54,15 @@ class TestFernsehserien(unittest.TestCase):
         test_date = datetime.date(2019, 1, 1)
         show_data = sensor.parseResponse(api_response, test_date)
 
-        show_data['episodes'] = sensor.filter_upcoming(show_data['episodes'], show, test_date)
         self.assertEqual(len(show_data['episodes']), 6)
 
     def test_correct_data(self):
         show = 'vikings'
         print("Test correct data for: " + show)
         api_response = requests.get(sensor.BASE_URL.format(show), timeout=10)
-        show_data = sensor.parseResponse(api_response)
         test_date = datetime.date(2019, 12, 1)
+        show_data = sensor.parseResponse(api_response, test_date)
 
-        show_data['episodes'] = sensor.filter_upcoming(show_data['episodes'], show, test_date)
         first_episode = show_data['episodes'][0]
         self.assertEqual(first_episode['seasonNumber'], 6)
         self.assertEqual(first_episode['episodeNumber'], 1)
